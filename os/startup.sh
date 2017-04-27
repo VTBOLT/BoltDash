75624167374fad77ae0a5405049714c326d7ea33
Eric Hahn <erichahn@vt.edu>
2017-04-27 15:59:01 -0400

    added startup

--- os/startup.sh
+++ os/startup.sh
@@ -1,5 +1,5 @@
-screen -d -m -S cansetup bash -c 'source /home/dash/.bashrc; setupcan; exec bash'
-screen -d -m -S roscore bash -c 'source /home/dash/dash/os/catkin_ws/devel/setup.bash ; roscore; exec bash'
-screen -d -m -S gpio bash -c 'source /home/dash/dash/os/catkin_ws/devel/setup.bash; rosrun gpio gpio_node; exec bash'
-screen -d -m -S can bash -c 'source /home/dash/dash/os/catkin_ws/devel/setup.bash; rosrun can_to_qt_bolt3 can_talker_bolt3; exec bash'
-screen -d -m -S qt bash -c './BoltDash; exec bash'
+#!/bin/bash
+screen -d -m -S roscore -Q select . ;  screen -dmS roscore bash -c 'source /home/dash/dash/catkin_ws/devel/setup.bash; roscore; exec bash';
+screen -d -m -S gpio -Q select . ; screen -dmS gpio bash -c 'source /home/dash/dash/catkin_ws/devel/setup.bash; rosrun gpio gpio_node; exec bash';
+screen -d -m -S can -Q select . ; screen -dmS can bash -c 'source /home/dash/dash/catkin_ws/devel/setup.bash; rosrun can_to_qt_bolt3 can_talker_bolt3; exec bash';
+screen -d -m -S qt -Q select . ; screen -dmS qt bash -c '/home/dash/BoltDash; exec bash';
