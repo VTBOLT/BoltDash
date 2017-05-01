203cf73d3d0bd544dbfc7ae534ac8b578b38e98d
Eric Hahn <erichahn@vt.edu>
2017-05-01 13:12:49 -0400

    update startup script after sam tested at 11 am today

--- os/startup.sh
+++ os/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-screen -d -m -S roscore -Q select . ;  screen -dmS roscore bash -c 'source /home/dash/dash/catkin_ws/devel/setup.bash; roscore; exec bash';
-screen -d -m -S gpio -Q select . ; screen -dmS gpio bash -c 'source /home/dash/dash/catkin_ws/devel/setup.bash; rosrun gpio gpio_node; exec bash';
-screen -d -m -S can -Q select . ; screen -dmS can bash -c 'source /home/dash/dash/catkin_ws/devel/setup.bash; rosrun can_to_qt_bolt3 can_talker_bolt3; exec bash';
-screen -d -m -S qt -Q select . ; screen -dmS qt bash -c '/home/dash/BoltDash; exec bash';
+screen -d -m -S roscore -Q select . ;  screen -dmS roscore bash -c 'source /home/dash/dash/catkin_ws/devel/setup.bash; roscore; exec bash;';
+screen -d -m -S gpio -Q select . ; screen -dmS gpio bash -c 'source /home/dash/dash/catkin_ws/devel/setup.bash; rosrun gpio gpio_node; exec bash;';
+screen -d -m -S can -Q select . ; screen -dmS can bash -c 'source /home/dash/dash/catkin_ws/devel/setup.bash; rosrun can_to_qt_bolt3 can_talker_bolt3; exec bash;';
+screen -d -m -S qt -Q select . ; screen -dmS qt bash -c '/home/dash/BoltDashDebug; exec bash;';
