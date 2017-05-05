fcbcbe6f695e83a8579bb452e56feb57c2de8ef4
Eric Hahn <erichahn@vt.edu>
2017-05-05 16:38:45 -0400

    debug printout on listen topic

--- catkin_ws/src/can_to_qt_bolt3/src/can_listener_bolt3.cpp
+++ catkin_ws/src/can_to_qt_bolt3/src/can_listener_bolt3.cpp
@@ -11,17 +11,25 @@
 
 #include "shared_messages.h"
 
+ros::NodeHandle n;
+std_msgs::String listen_message;
+ros::Publisher listener = n.advertise<std_msgs::String>("listen", 100);
+
 
 void chatterCallback_can_msg(const can_to_qt_bolt3::can_msg::ConstPtr& msg)
 {
   //ROS_INFO("motor [can_id]: [%i] rpm: %i RMS_currentL %i DC_voltage: %i drive6stat: %i", msg->can_id, msg->rpm, msg->RMS_current, msg->DC_voltage, msg->drive6stat);
   std::cout << msg->define << ";" << msg->can_data << std::endl;
+  listen_message.data = msg->define + ";" + msg->can_data + '\n';
+  listener.publish(listen_message);
 }
 
 void chatterCallback_fault_msg(const can_to_qt_bolt3::fault_msg::ConstPtr& msg)
 {
   //ROS_INFO("motor [can_id]: [%i] rpm: %i RMS_currentL %i DC_voltage: %i drive6stat: %i", msg->can_id, msg->rpm, msg->RMS_current, msg->DC_voltage, msg->drive6stat);
   std::cout << msg->define << ";" << msg->msg_data << std::endl;
+  listen_message.data = msg->define + ";" + msg->msg_data + '\n';
+  listener.publish(listen_message);
 }
 
 void gpio_callback(const can_to_qt_bolt3::gpio_msg::ConstPtr& msg)
@@ -30,17 +38,20 @@ void gpio_callback(const can_to_qt_bolt3::gpio_msg::ConstPtr& msg)
   std::cout << gpio_IMD << ";" << (int)msg->IMD <<std::endl;
   std::cout << gpio_PRESSURE << ";" << (int)msg->PRESSURE <<std::endl;
   std::cout << gpio_BMSDE << ";" << (int)msg->BMSDE <<std::endl;
+
+  listen_message.data = (int)msg->IGNOK + (int)msg->IMD + (int)msg->PRESSURE + (int)msg->BMSDE + '\n';
+  listener.publish(listen_message);
 }
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "can_listener_bolt3");
-
-  ros::NodeHandle n;
-
+  
   ros::Subscriber sub1 = n.subscribe("/can_msg", 1000, chatterCallback_can_msg);
   ros::Subscriber sub2 = n.subscribe("/fault_msg", 1000, chatterCallback_fault_msg);
   ros::Subscriber gpio = n.subscribe("/gpio/all", 100, gpio_callback);
+  
+
 	
   ros::spin();
 
